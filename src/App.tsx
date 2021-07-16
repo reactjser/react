f4e5aa24fff03da2d33255aa43cce4898068db8e
liuyao18 <liuyao18@baijia.com>
2021-07-16 12:36:56 +0800

    feat: react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import { QueryClient, QueryClientProvider } from 'react-query';
 import { NavLink, useRoutes } from 'react-router-dom';
 
 import { routes } from './router';
 
+const queryClient = new QueryClient();
+
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <div className="nav">
         <NavLink to="/" end>
           Home
@@ -13,7 +16,7 @@ function App() {
         <NavLink to="about">About</NavLink>
       </div>
       {useRoutes(routes)}
-    </>
+    </QueryClientProvider>
   );
 }
 
--- src/views/About.tsx
+++ src/views/About.tsx
@@ -1,10 +1,24 @@
-import { Button } from 'antd';
 import React from 'react';
+import { useQuery } from 'react-query';
 
 function About() {
+  const { isLoading, error, data } = useQuery<any, Error>('repoData', () =>
+    fetch('https://api.github.com/repos/tannerlinsley/react-query').then(
+      (res) => res.json(),
+    ),
+  );
+
+  if (isLoading) return <div>Loading...</div>;
+
+  if (error) return <div>An error has occurred: {error.message}</div>;
+
   return (
-    <div className="about">
-      <Button type="primary">About Page</Button>
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{' '}
+      <strong>✨ {data.stargazers_count}</strong>{' '}
+      <strong>🍴 {data.forks_count}</strong>
     </div>
   );
 }
